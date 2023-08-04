5555fe23a0828565600cfd085a90799f71a35090
Đinh Ngọc Tâm <tamdinh572003@gmail.com>
2023-08-04 16:01:39 +0700

    Day 1 learn JS

---
+++ JS-Dom/app.js
@@ -0,0 +1,18 @@
+const userList = document.querySelector(".name-list");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
+
+addListBtn.addEventListener("click", function () {
+    //Create an li out of thin air
+    const newLi = document.createElement('li');
+    
+    const liContent = document.createTextNode(listInput.value);
+    
+    //Add the input value inside that new li
+    
+    newLi.appendChild(liContent);
+    //Attaching the li to the user list
+    newLi.style.color = "green";
+    userList.appendChild(newLi);
+});
+
